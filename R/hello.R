7c0aa9138df6b43de89cded8d5afcd24894f6c20
Russ Hyde <russ@jumpingrivers.com>
2026-04-13 09:19:02 +0100

    add temporary function hello()

---
+++ R/hello.R
@@ -0,0 +1,9 @@
+#' Temporary function, until we have some real widgets in the package
+#'
+#' @param   x   Scalar character. A name to be greeted.
+#' @return   NULL
+#' @export
+
+hello <- function(x = "World") {
+  message("Hello, ", x, "!")
+}
