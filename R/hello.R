204a8584cae738c1f533cb61537dc78523910177
Russ Hyde <russ@jumpingrivers.com>
2026-04-22 16:00:46 +0100

    remove temporary hello() function

--- R/hello.R
+++
@@ -1,9 +0,0 @@
-#' Temporary function, until we have some real widgets in the package
-#'
-#' @param   x   Scalar character. A name to be greeted.
-#' @return   NULL
-#' @export
-
-hello <- function(x = "World") {
-  message("Hello, ", x, "!")
-}
