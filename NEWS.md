1c886953358a155970288dddbdd55657bdbd57ba
Russ Hyde <russHyde@users.noreply.github.com>
2026-04-27 13:30:11 +0100

    Map data (#3)
    * env: add suggested deps, required for making map-data objects
    
    * copy map-generating code/data from {custom.map}
    
    * refac: put json shapefiles where they won't get installed/built with the package
    
    * add data reqd for us_map3
    
    * add data reqd for us_map (needed to make us_map3)
    
    * data: only save the us_map3 data that is needed by governance page maps
    
    * set lower bound for R version (since data-prep scripts need \(){} syntax
    
    * export the us_map3 dataset for use in asthoWidgets2025
    
    * remove temporary hello() function
    
    * refac: clarify the code that makes us_map and us_map3
    
    * refac: rename the file that prepares the us_map objects
    
    * doc: where is us_map3 used
    
    * create us_map2, as used in finance and covid pages of asthoProfile
    
    * remove a comment
    
    * data: add us_map2
    
    * remove test for hello() function
    
    * chore: bump version
    
    * refac: style files and fix lints
    
    * build: drop data-raw/ from package build step

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# asthoWidgets 0.0.1
+
+* Add us_map2 and us_map3 data-objects; these can be used to generate highchart maps.
---
+++ data-raw/us_map.R
@@ -0,0 +1,62 @@
+library(dplyr)
+
+sh_dir <- file.path("data-raw", "json-shapefiles")
+m <- jsonlite::read_json(file.path(sh_dir, "mapdata.json"))
+m2 <- jsonlite::read_json(file.path(sh_dir, "mapdata2.json"))
+m3 <- jsonlite::read_json(file.path(sh_dir, "mapdata3.json"))
+m4 <- jsonlite::read_json(file.path(sh_dir, "mapdata4.json"))
+
+# Ensure 'fips' is an integer
+# Ensure region shape/geometry comes from mapdata2.json
+us_map <- purrr::map2(
+  m[[1]]$mapData,
+  m2[[1]]$mapData,
+  function(region, new_region) {
+    region$fips <- as.numeric(region$fips)
+    region$path <- new_region$path
+
+    region
+  }
+)
+
+# - Used in finance and covid pages
+us_map2 <- purrr::map(
+  m3[[1]]$mapData,
+  function(region) {
+    region$fips <- as.numeric(region$fips)
+
+    region
+  }
+)
+
+# Ensure `id`, `name` and `fips` matches between `us_map3` and `us_map`
+# - Used in governance, structure, activities pages
+us_map3 <- purrr::map(
+  m4[[1]]$mapData,
+  function(region) {
+    # IDs are mismatched between `us_map` and `mapdata4.json`
+    shift_region_id <- function(x) {
+      paste0("id", -1 + as.integer(gsub("id", "", x)))
+    }
+    region$id <- shift_region_id(region$id)
+
+    # Get the matching region from `us_map`
+    cn <- us_map |>
+      purrr::keep(function(x) {
+        x$id == region$id
+      })
+
+    if (length(cn) == 0) {
+      cat("Could not find:", region$id, "\n")
+      return(region)
+    }
+
+    region$fips <- as.numeric(cn[[1]]$fips)
+    region$name <- cn[[1]]$name
+
+    region
+  }
+)
+
+usethis::use_data(us_map2, overwrite = TRUE)
+usethis::use_data(us_map3, overwrite = TRUE)
